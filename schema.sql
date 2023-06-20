7fc7740629848ec39b6a2f620f82e57e875536e0
JAVED SHAHBAZ <76252961+Javed-Shahbaz@users.noreply.github.com>
2023-06-21 01:42:45 +0500

    create table

--- schema.sql
+++ schema.sql
@@ -1,9 +1,3 @@
 CREATE DATABASE vet_clinic
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
+create table animals (id integer, name varchar(20), data_of_birth date,escape_attempts integer,neutered boolean,weight_kg decimal);
 
-COMMENT ON DATABASE vet_clinic
-    IS 'Animal Database';
