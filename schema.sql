93e456499e8de61ebecf11822080d573fbf80c56
JAVED SHAHBAZ <76252961+Javed-Shahbaz@users.noreply.github.com>
2023-06-24 00:43:51 +0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,4 +2,4 @@ CREATE DATABASE vet_clinic
 create table animals (id integer, name varchar(20), data_of_birth date,escape_attempts integer,neutered boolean,weight_kg decimal);
 
 alter table animals 
-add species varchar;
+add species varchar(255);
