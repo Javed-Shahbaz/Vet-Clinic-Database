ba2a90869bd354f7d7aeae4c69dabe1c3582cf3e
JAVED SHAHBAZ <76252961+Javed-Shahbaz@users.noreply.github.com>
2023-06-24 14:31:45 +0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,3 +3,11 @@ create table animals (id integer, name varchar(20), data_of_birth date,escape_at
 
 alter table animals 
 add species varchar(255);
+
+alter table animals alter column id set default nextval('animals');
+
+select * from animals;
+alter table animals add column species_id integer;
+alter table animals add constraint fk_id foreign key (species_id) references species(id);
+alter table animals add column owner_id integer;
+alter table animals add constraint fko_id foreign key (owner_id) references owners(id);
