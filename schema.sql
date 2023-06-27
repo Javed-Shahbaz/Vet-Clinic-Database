7013f119d2bdf5c2752ba7e8e18ab8e0c3594680
JAVED SHAHBAZ <76252961+Javed-Shahbaz@users.noreply.github.com>
2023-06-27 21:00:31 +0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -50,3 +50,23 @@ ADD FOREIGN KEY (vet_id) REFERENCES vets (id);
 
 ALTER TABLE visits
 ADD COLUMN visit_date date;
+
+ALTER TABLE animals
+ADD COLUMN visits_total int DEFAULT 0;
+
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+-- ADD INDEX ON email in owners TABLE TO REDUCE EXECUTION TIME
+CREATE INDEX owner_email_IDX ON owners (email);
+
+-- CREATE TABLE Vet_Sumary TO STORE TOTAL VISITS OF EVERY VET
+CREATE TABLE Vet_Sumary (
+  vet_id int REFERENCES vets(id), 
+  total_visits int
+);
+
